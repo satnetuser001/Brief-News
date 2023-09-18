0887af99917ca4a661a993da99d6e81f6bd6e4a8
Anton Yurchenko <satnetuser001@gmail.com>
2023-09-18 22:38:01 +0300

    Added styles for the page "Create Article"

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -3,92 +3,163 @@
 @section('title', 'Создать статью')
 
 @section('content')
-	<div>
+<div class="createArticlePage">
+	<div class="namePage">
 		<b>Создать статью</b>
 	</div>
 
-	<form action="{{ route('articles.store') }}" method="POST">
+	<form id="createArticleForm" action="{{ route('articles.store') }}" method="POST">
 
 		@csrf
 
-		<div>
-			<label>Выберите рубрики и локаль для статьи</label><br>
-			<input type="checkbox" name="arrRubricsCombination[policy]" value="1"
-				@if(old('arrRubricsCombination.policy') == 1)
-					checked
-				@endif
-			>policy 
-			<input type="checkbox" name="arrRubricsCombination[economy]" value="1"
-				@if(old('arrRubricsCombination.economy') == 1)
-					checked
-				@endif
-			>economy 
-			<input type="checkbox" name="arrRubricsCombination[science]" value="1"
-				@if(old('arrRubricsCombination.science') == 1)
-					checked
-				@endif
-			>science 
-			<input type="checkbox" name="arrRubricsCombination[technologies]" value="1"
-				@if(old('arrRubricsCombination.technologies') == 1)
-					checked
-				@endif
-			>technologies 
-			<input type="checkbox" name="arrRubricsCombination[sport]" value="1"
-				@if(old('arrRubricsCombination.sport') == 1)
-					checked
-				@endif
-			>sport 
-			<input type="checkbox" name="arrRubricsCombination[other]" value="1"
-				@if(old('arrRubricsCombination.other') == 1)
-					checked
-				@endif
-			>other 
-			<input type="checkbox" name="arrLocaleCombination[world]" value="1"
-				@if(old('arrLocaleCombination.world') == 1)
-					checked
-				@endif
-			>world 
-			<input type="checkbox" name="arrLocaleCombination[local]" value="1"
-				@if(old('arrLocaleCombination.local') == 1)
-					checked
-				@endif
-			>local
+		<div class="rubricsAndLocaleSelectionSection">
+			<label><b>Рубрики и локаль</b></label>
+			<div class="rubricsAndLocale">
+				<div class="rubric">
+					<input
+						type="checkbox"
+						name="arrRubricsCombination[policy]"
+						value="1"
+						@if(old('arrRubricsCombination.policy') == 1)
+							checked
+						@endif
+					>
+					<div>Политика</div>
+				</div>
+				<div class="rubric">
+					<input
+						type="checkbox"
+						name="arrRubricsCombination[economy]"
+						value="1"
+						@if(old('arrRubricsCombination.economy') == 1)
+							checked
+						@endif
+					>
+					<div>Экономика</div>
+				</div>
+				<div class="rubric">
+					<input
+						type="checkbox"
+						name="arrRubricsCombination[science]"
+						value="1"
+						@if(old('arrRubricsCombination.science') == 1)
+							checked
+						@endif
+					>
+					<div>Наука</div>
+				</div>
+				<div class="rubric">
+					<input
+						type="checkbox"
+						name="arrRubricsCombination[technologies]"
+						value="1"
+						@if(old('arrRubricsCombination.technologies') == 1)
+							checked
+						@endif
+					>
+					<div>Технологии</div>
+				</div>
+				<div class="rubric">
+					<input
+						type="checkbox"
+						name="arrRubricsCombination[sport]"
+						value="1"
+						@if(old('arrRubricsCombination.sport') == 1)
+							checked
+						@endif
+					>
+					<div>Спорт</div>
+				</div>
+				<div class="rubric">
+					<input
+						type="checkbox"
+						name="arrRubricsCombination[other]"
+						value="1"
+						@if(old('arrRubricsCombination.other') == 1)
+							checked
+						@endif
+					>
+					<div>Остальные</div>
+				</div>
+				<div class="locale">
+					<input
+						type="checkbox"
+						name="arrLocaleCombination[world]"
+						value="1"
+						@if(old('arrLocaleCombination.world') == 1)
+							checked
+						@endif
+					>
+					<div>Мировые</div>
+				</div>
+				<div class="locale">
+					<input
+						type="checkbox"
+						name="arrLocaleCombination[local]"
+						value="1"
+						@if(old('arrLocaleCombination.local') == 1)
+							checked
+						@endif
+					>
+					<div>Локальные</div>
+				</div>
+			</div>
+			<div class="errorMessage">
+				@error('arrRubricsCombination')
+					<span ><strong>{{ $message }}</strong></span>
+				@enderror
+				@error('arrLocaleCombination')
+					<span ><strong>{{ $message }}</strong></span>
+				@enderror
+			</div>
 		</div>
-		@error('arrRubricsCombination')
-			<span ><strong>{{ $message }}</strong></span><br>
-		@enderror
-		@error('arrLocaleCombination')
-			<span ><strong>{{ $message }}</strong></span>
-		@enderror
 
-		<div>
-			<label>Заголовок</label>
-			<input name="header" value="{{ old('header') }}">
-			@error('header')
-				<span><strong>{{ $message }}</strong></span>
-			@enderror
+		<div class="header">
+			<label><b>Заголовок</b></label>
+			<input name="header" value="{{ old('header') }}" class="articleHeaderInput">
+			<div class="errorMessage">
+				@error('header')
+					<span><strong>{{ $message }}</strong></span>
+				@enderror
+			</div>
 		</div>
 
-		<div>
-			<label>Статья</label>
-			<textarea name="body">{{ old('body') }}</textarea>
-			@error('body')
-			<span ><strong>{{ $message }}</strong></span>
-			@enderror
+		<div class="body">
+			<label><b>Статья</b></label>
+			<textarea name="body" class="articleBodyInput">{{ old('body') }}</textarea>
+			<div class="errorMessage">
+				@error('body')
+					<span ><strong>{{ $message }}</strong></span>
+				@enderror
+			</div>
 		</div>
 
-		<div>
-			<div name='links' id='links'></div>
-			<input type='button' value='Добавить ссылку на источник' onclick="addLinkInput()">
+		<div class="sourcesLinks">
+			<label><b>Ссылки на источники</b></label>
+			<div>
+				<div name='links' id='links'></div>
+				<input
+					type='button'
+					value='Добавить ссылку'
+					onclick="addLinkInput()"
+					class="button htmlButtonStyleCorrection"
+				>
+			</div>
+			<div>
+				<script type="text/javascript">
+					let currentNumber = 0; 
+						function addLinkInput(){
+					    document.getElementById('links').innerHTML += "<input type='text' name='links[" + currentNumber + "]' /><br/>";
+					    currentNumber++;
+					}
+				</script>
+			</div>
 		</div>
-		<script type="text/javascript">
-			let currentNumber = 0; 
-				function addLinkInput(){
-			    document.getElementById('links').innerHTML += "<input type='text' name='links[" + currentNumber + "]' /><br/>";
-			    currentNumber++;
-			}
-		</script>
-		<input type="submit" value="Сохранить">
 	</form>
-	<a href="{{ route('home') }}">Отмена</a>
+
+	<div class="saveCancel">
+		<button type="submit" form="createArticleForm" class="button htmlButtonStyleCorrection">Сохранить</button>
+		<a href="{{ route('home') }}" class="button">Отмена</a>		
+	</div>
+</div>
 @endsection
