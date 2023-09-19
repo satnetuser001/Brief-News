6e77ac6df803ae326d6d5cdb385c7eaf9a98272b
Anton Yurchenko <satnetuser001@gmail.com>
2023-09-19 18:20:15 +0300

    Added styles for the page "Delete Article".

--- resources/views/articles/destroy.blade.php
+++ resources/views/articles/destroy.blade.php
@@ -3,38 +3,27 @@
 @section('title', 'Удалить статью')
 
 @section('content')
-	<div>
-		<b>Вы действительно хотите удалить статью?</b>
+<!-- Reused styles from different pages -->
+<div class="createArticlePage">
+	<div class="namePage">
+		<b>Удалить статью?</b>
 	</div>
+	
+	<label class="deleteArticleHeader">
+	    <div class="articleCreationDate">{{ $context->created_at }}</div>
+	    <div class="articleName">
+	        <b>{{ $context->header }}</b>
+	    </div>
+	</label>
 
-	<div>
-		{{ $context->header }}
+	<div class="saveCancel">
+		<form action="{{ route('articles.destroy', [$context->id]) }}" method="POST">
+			@csrf
+			@method('DELETE')
+			<input type="submit" value="Удалить" class="button htmlButtonStyleCorrection">
+		</form>
+
+		<a href="{{ route('home') }}" class="button">Отмена</a>
 	</div>
-
-	<div>
-		<b>Автор:</b>
-		<div>
-			ID: {{ $context->user->id }}
-		</div>
-
-		@isset($context->user->surname)
-			<div>
-				Фамилия: {{ $context->user->surname }}
-			</div>
-		@endisset
-
-		@isset($context->user->name)
-			<div>
-				Имя: {{ $context->user->name }}
-			</div>
-		@endisset
-	</div>
-
-	<form action="{{ route('articles.destroy', [$context->id]) }}" method="POST">
-		@csrf
-		@method('DELETE')
-		<input type="submit" value="Удалить">
-	</form>
-
-	<a href="{{ route('home') }}">Отмена</a>
+</div>
 @endsection
