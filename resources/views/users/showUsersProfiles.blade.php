3e54e222ae2f6c246aa843313b65a4b0292bb425
Anton Yurchenko <satnetuser001@gmail.com>
2023-09-20 20:03:03 +0300

    Added styles for the pages "All Profiles" "Trashed Users Profiles".

--- resources/views/users/showUsersProfiles.blade.php
+++ resources/views/users/showUsersProfiles.blade.php
@@ -10,7 +10,8 @@
         
 
 @section('content')
-    <div>
+<div class="showUserProfiles">
+    <div class="namePage">
         <b>
             @if(Route::is('users.allProfiles'))
                 Все профили
@@ -22,67 +23,82 @@
         </b>
     </div>
 
-    @if(count($context) > 0)
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Имя</th>
-                    <th>Фамилия</th>
-                    <th>Псевдоним</th>
-                    <th>Роль</th>
-                    <th>Статус</th>
-                    <th>Email</th>
-                    <th>Дата создания</th>
-                    <th>&nbsp</th>
-                    @if(Route::is('users.allProfiles'))
-                        <th>&nbsp</th>
-                    @endif
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($context as $user)
+    <div>
+        @if(count($context) > 0)
+            <table class="usersTable">
+                <thead>
                     <tr>
-                        <td>{{ $user->id }}</td>
-                        <td>{{ $user->name }}</td>
-                        <td>{{ $user->surname }}</td>
-                        <td>{{ $user->nickname }}</td>
-                        <td>{{ $user->role }}</td>
-                        <td>{{ $user->status }}</td>
-                        <td>{{ $user->email }}</td>
-                        <td>{{ $user->created_at }}</td>
+                        <th>ID</th>
+                        <th>Имя</th>
+                        <th>Фамилия</th>
+                        <th>Псевдоним</th>
+                        <th>Роль</th>
+                        <th>Статус</th>
+                        <th>Email</th>
+                        <th>Дата создания</th>
+                        <th>&nbsp</th>
                         @if(Route::is('users.allProfiles'))
-                            @if($user->role != "root" or Auth::user()->role == "root")
-                                <td>
-                                    <a href="{{ route('users.editUserProfile', ['user'=>$user->id]) }}">Редактировать</a>
-                                </td>
-                            @else
-                                <td>&nbsp</td>
-                            @endif
-                            @if($user->role != "root")
-                                <td>
-                                    <a href="{{ route('users.destroyUserProfileConfirm', ['user'=>$user->id]) }}">Удалить</a>
-                                </td>
-                            @else
-                                <td>&nbsp</td>
-                            @endif
-                        @elseif(Route::is('users.trashedUsersProfiles'))
-                            <td>
-                                <form action="{{ route('users.restoreUserProfile', ['id'=>$user->id]) }}" method="POST">
-                                    @csrf
-                                    @method('PATCH')
-                                    <input type="submit" value="Восстановить">
-                                </form>
-                            </td>
+                            <th>&nbsp</th>
                         @endif
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
-    @endif
+                </thead>
+                <tbody>
+                    @foreach($context as $user)
+                        <tr
+                            @if($loop->odd)
+                                class="oddRow"
+                            @endif
+                        >
+                            <td>{{ $user->id }}</td>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->surname }}</td>
+                            <td>{{ $user->nickname }}</td>
+                            <td>{{ $user->role }}</td>
+                            <td>{{ $user->status }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>{{ $user->created_at }}</td>
+                            @if(Route::is('users.allProfiles'))
+                                @if($user->role != "root" or Auth::user()->role == "root")
+                                    <td>
+                                        <a
+                                            href="{{ route('users.editUserProfile', ['user'=>$user->id]) }}"
+                                            class="buttonInTable"
+                                        >Редактировать</a>
+                                    </td>
+                                @else
+                                    <td>&nbsp</td>
+                                @endif
+                                @if($user->role != "root")
+                                    <td>
+                                        <a
+                                            href="{{ route('users.destroyUserProfileConfirm', ['user'=>$user->id]) }}"
+                                            class="buttonInTable"
+                                        >Удалить</a>
+                                    </td>
+                                @else
+                                    <td>&nbsp</td>
+                                @endif
+                            @elseif(Route::is('users.trashedUsersProfiles'))
+                                <td>
+                                    <form action="{{ route('users.restoreUserProfile', ['id'=>$user->id]) }}" method="POST">
+                                        @csrf
+                                        @method('PATCH')
+                                        <input
+                                            type="submit"
+                                            value="Восстановить"
+                                            class="buttonInTable restoreButtonInTableCorrection"
+                                        >
+                                    </form>
+                                </td>
+                            @endif
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @endif
+    </div>
 
-    <!-- paginator -->
-    <!-- need to add "if" to check if paginator method exists in object -->
-    <div>{{ $context->links() }}</div>
-    
+    <!-- pagination -->
+    {{ $context->links() }}
+</div>
 @endsection
