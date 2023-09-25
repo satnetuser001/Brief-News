737af77615f6370090a3ea00aacae35b74ef2228
Anton Yurchenko <satnetuser001@gmail.com>
2023-09-25 15:17:34 +0300

    Russifying the application. Have style bug on pages: login, register, edit user password.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -54,7 +54,7 @@ class RegisterController extends Controller
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
-    }
+    } 
 
     /**
      * Create a new user instance after a valid registration.
--- resources/views/users/editPassword.blade.php
+++ resources/views/users/editPassword.blade.php
@@ -3,46 +3,71 @@
 @section('title', 'Изменить пароль')
 
 @section('content')
-    <div>
+<!-- Reused styles from Login page -->
+<div class="loginPage">
+
+    <div class="namePage">
         <b>Изменить пароль</b>
     </div>
 
-    <form action="{{ route('users.updatePassword', [$context['id']]) }}" method="POST">
+    <form
+        id="editPasswordForm"
+        action="{{ route('users.updatePassword', [$context['id']]) }}"
+        method="POST"
+        class="loginForm"
+    >
 
         @csrf
         @method('PATCH')
 
         <!-- enter old password -->
-        <div>
-            <label>Введите текущий пароль:</label>
-            <input type="password" name="currentPassword" >
-            @error('currentPassword')
-                <span><strong>{{ $message }}</strong></span>
-            @enderror
-        </div>
+        <div class="editPasswordValueSection">
+            <div class="loginInputSection">
+                <label>Введите текущий пароль:</label>
+                <input type="password" name="currentPassword" class="loginInput">
+            </div>
+            <div class="errorMessage">
+                @error('currentPassword')
+                    <strong>{{ $message }}</strong>
+                @enderror
+            </div>
+        </div>  
 
         <!-- enter new password -->
-        <div>
-            <label>Введите новый пароль:</label>
-            <input type="password" name="newPassword" >
-            @error('newPassword')
-                <span><strong>{{ $message }}</strong></span>
-            @enderror
+        <div class="editPasswordValueSection">
+            <div class="loginInputSection">
+                <label>Введите новый пароль:</label>
+                <input type="password" name="newPassword" class="loginInput">
+            </div>
+            <div class="errorMessage">
+                @error('newPassword')
+                    <span><strong>{{ $message }}</strong></span>
+                @enderror
+            </div>
         </div>
 
         <!-- enter new password again -->
-        <div>
-            <label>Введите новый пароль еще раз:</label>
-            <input type="password" name="сonfirmNewPassword" >
-            @error('сonfirmNewPassword')
-                <span><strong>{{ $message }}</strong></span>
-            @enderror
+        <div class="editPasswordValueSection">
+            <div>
+                <label>Введите новый пароль еще раз:</label>
+                <input type="password" name="сonfirmNewPassword" class="loginInput">
+            </div>
+            <div class="errorMessage">
+                @error('сonfirmNewPassword')
+                    <span><strong>{{ $message }}</strong></span>
+                @enderror
+            </div>
         </div>
-
-        <!-- save -->
-        <input type="submit" value="Сохранить">
     </form>
 
-    <!-- cancel -->
-    <a href="{{ route('users.myProfile') }}">Отмена</a>
+    <!-- save and cancel -->
+    <div class="editPasswordSaveCancel">
+        <button
+            form="editPasswordForm"
+            type="submit"
+            class="button htmlButtonStyleCorrection"
+        >Сохранить</button>
+        <a href="{{ route('users.myProfile') }}" class="button">Отмена</a>
+    </div>
+</div>
 @endsection
