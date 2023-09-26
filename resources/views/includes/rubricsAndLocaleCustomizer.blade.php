1eb9a8ceca8c900610ce53b58012e04ad74505bd
Anton Yurchenko <satnetuser001@gmail.com>
2023-09-26 19:27:41 +0300

    Added styles for the pages "Edit User Profile".

--- resources/views/includes/rubricsAndLocaleCustomizer.blade.php
+++ resources/views/includes/rubricsAndLocaleCustomizer.blade.php
@@ -1,49 +1,150 @@
-<div>
-    <label>Выберите рубрики и локаль отображаемые по умолчанию на Главной странице</label><br>
-    <input type="checkbox" name="arrRubricsCombination[policy]" value="1"
-        @if(old('arrRubricsCombination.policy') == 1 or (!old() and $context['rubricsCombination']['policy'] ==1))
-            checked
-        @endif
-    >policy 
-    <input type="checkbox" name="arrRubricsCombination[economy]" value="1"
-        @if(old('arrRubricsCombination.economy') == 1 or (!old() and $context['rubricsCombination']['economy'] ==1))
-            checked
-        @endif
-    >economy 
-    <input type="checkbox" name="arrRubricsCombination[science]" value="1"
-        @if(old('arrRubricsCombination.science') == 1 or (!old() and $context['rubricsCombination']['science'] ==1))
-            checked
-        @endif
-    >science 
-    <input type="checkbox" name="arrRubricsCombination[technologies]" value="1"
-        @if(old('arrRubricsCombination.technologies') == 1 or (!old() and $context['rubricsCombination']['technologies'] ==1))
-            checked
-        @endif
-    >technologies 
-    <input type="checkbox" name="arrRubricsCombination[sport]" value="1"
-        @if(old('arrRubricsCombination.sport') == 1 or (!old() and $context['rubricsCombination']['sport'] ==1))
-            checked
-        @endif
-    >sport 
-    <input type="checkbox" name="arrRubricsCombination[other]" value="1"
-        @if(old('arrRubricsCombination.other') == 1 or (!old() and $context['rubricsCombination']['other'] ==1))
-            checked
-        @endif
-    >other 
-    <input type="checkbox" name="arrLocaleCombination[world]" value="1"
-        @if(old('arrLocaleCombination.world') == 1 or (!old() and $context['rubricsCombination']['world'] ==1))
-            checked
-        @endif
-    >world 
-    <input type="checkbox" name="arrLocaleCombination[local]" value="1"
-        @if(old('arrLocaleCombination.local') == 1 or (!old() and $context['rubricsCombination']['local'] ==1))
-            checked
-        @endif
-    >local
+<!-- Reused styles from the "Create Article Page" -->
+<div class="rubricsAndLocaleSelectionSection">
+    <label><b>Выберите рубрики и локаль отображаемые по умолчанию на Главной странице</b></label>
+    <div class="rubricsAndLocale">
+        <div class="rubric">
+            <input
+                type="checkbox"
+                name="arrRubricsCombination[policy]"
+                value="1"
+                @if(
+                        old('arrRubricsCombination.policy') == 1 or
+                        (
+                            !old() and
+                            $context['rubricsCombination']['policy'] ==1
+                        )
+                    )
+                    checked
+                @endif
+            >
+            <div>Политика</div>
+        </div>
+        <div class="rubric">
+            <input
+                type="checkbox"
+                name="arrRubricsCombination[economy]"
+                value="1"
+                @if(
+                        old('arrRubricsCombination.economy') == 1 or
+                        (
+                            !old() and
+                            $context['rubricsCombination']['economy'] ==1
+                        )
+                    )
+                    checked
+                @endif
+            >
+            <div>Экономика</div>
+        </div>
+        <div class="rubric">
+            <input
+                type="checkbox"
+                name="arrRubricsCombination[science]"
+                value="1"
+                @if(
+                        old('arrRubricsCombination.science') == 1 or
+                        (
+                            !old() and
+                            $context['rubricsCombination']['science'] ==1
+                        )
+                    )
+                    checked
+                @endif
+            >
+            <div>Наука</div>
+        </div>
+        <div class="rubric">
+            <input
+                type="checkbox"
+                name="arrRubricsCombination[technologies]"
+                value="1"
+                @if(
+                        old('arrRubricsCombination.technologies') == 1 or
+                        (
+                            !old() and
+                            $context['rubricsCombination']['technologies'] ==1
+                        )
+                    )
+                    checked
+                @endif
+            >
+            <div>Технологии</div>
+        </div>
+        <div class="rubric">
+            <input
+                type="checkbox"
+                name="arrRubricsCombination[sport]"
+                value="1"
+                @if(
+                        old('arrRubricsCombination.sport') == 1 or
+                        (
+                            !old() and
+                            $context['rubricsCombination']['sport'] ==1
+                        )
+                    )
+                    checked
+                @endif
+            >
+            <div>Спорт</div>
+        </div>
+        <div class="rubric">
+            <input
+                type="checkbox"
+                name="arrRubricsCombination[other]"
+                value="1"
+                @if(
+                        old('arrRubricsCombination.other') == 1 or
+                        (
+                            !old() and
+                            $context['rubricsCombination']['other'] ==1
+                        )
+                    )
+                    checked
+                @endif
+            >
+            <div>Остальные</div>
+        </div>
+        <div class="locale">
+            <input
+                type="checkbox"
+                name="arrLocaleCombination[world]"
+                value="1"
+                @if(
+                        old('arrLocaleCombination.world') == 1 or
+                        (
+                            !old() and
+                            $context['rubricsCombination']['world'] ==1
+                        )
+                    )
+                    checked
+                @endif
+            >
+            <div>Мировые</div>
+        </div>
+        <div class="locale">
+            <input
+                type="checkbox"
+                name="arrLocaleCombination[local]"
+                value="1"
+                @if(
+                        old('arrLocaleCombination.local') == 1 or
+                        (
+                            !old() and
+                            $context['rubricsCombination']['local'] ==1
+                        )
+                    )
+                    checked
+                @endif
+            >
+            <div>Локальные</div>
+        </div>
+    </div>
+    <div class="errorMessage">
+        @error('arrRubricsCombination')
+            <strong>{{ $message }}</strong>
+        @enderror
+        @error('arrLocaleCombination')
+            <strong>{{ $message }}</strong>
+        @enderror
+    </div>
 </div>
-@error('arrRubricsCombination')
-    <span ><strong>{{ $message }}</strong></span><br>
-@enderror
-@error('arrLocaleCombination')
-    <span ><strong>{{ $message }}</strong></span>
-@enderror
--- resources/views/users/editUserProfile.blade.php
+++ resources/views/users/editUserProfile.blade.php
@@ -3,11 +3,16 @@
 @section('title', 'Изменить профиль пользователя')
 
 @section('content')
-    <div>
+<div class="editUserProfile">
+    <div class="namePage">
         <b>Изменить профиль пользователя {{ $context['nickname'] }}</b>
     </div>
 
-    <form action="{{ route('users.updateUserProfile', [$context['id']]) }}" method="POST">
+    <form
+        id="editUserProfileForm"
+        action="{{ route('users.updateUserProfile', [$context['id']]) }}"
+        method="POST"
+    >
 
         @csrf
         @method('PATCH')
@@ -15,110 +20,168 @@
         <!-- Customization panel of rubrics and locale -->
         @include('includes.rubricsAndLocaleCustomizer')
 
-        <!-- ID -->
-        <div>
-            ID учетной записи: {{ $context['id'] }}
-        </div>
+        <div class="userDataRow">
 
-        <!-- created_at -->
-        <div>
-            Учетная запись создана: {{ $context['created_at'] }}
-        </div>
+            {{--
+            <div class="userDataColumnSection">
+                <div class="userDataColumn">
+                    <label>Some</label>
+                    <input type="" name=""> or <div>data</div>
+                </div>
+                <div class="loginErrorMessage">
+                    error
+                </div>
+            </div>
+            --}}
 
-        <!-- updated_at -->
-        <div>
-            Учетная запись обновлена: {{ $context['updated_at'] }}
-        </div>
+            <!-- ID -->
+            <div class="userDataColumnSection">
+                <div class="userDataColumn">
+                    <label>ID учетной записи:</label>
+                    <div>{{ $context['id'] }}</div>
+                </div>
+            </div>
 
-        <!-- role -->
-        <div>
-            <label>Роль:</label>
-            @if($context['role'] != 'root')
-                <select name="role">
-                    <option value="admin" @if($context['role'] == 'admin') selected @endif>admin</option>
-                    <option value="writer" @if($context['role'] == 'writer') selected @endif>writer</option>
-                    <option value="reader" @if($context['role'] == 'reader') selected @endif>reader</option>
-                </select>
-            @else
-                root
-                <input type="hidden" name="role" value="root">
-            @endif
-            @error('role')
-                <span><strong>{{ $message }}</strong></span>
-            @enderror
-        </div>
+            <!-- created_at -->
+            <div class="userDataColumnSection">
+                <div class="userDataColumn">
+                    <label>Учетная запись создана:</label>
+                    <div>{{ $context['created_at'] }}</div>
+                </div>
+            </div>
 
-        <!-- status -->
-        <div>
-            <label>Статус:</label>
-            @if($context['role'] != 'root' and $context['role'] != 'admin')
-                <select name="status">
-                    <option value="active" @if($context['status'] == 'active') selected @endif>активен</option>
-                    <option value="ban" @if($context['status'] == 'ban') selected @endif>бан</option>
-                </select>
-            @else
-                активен
-                <input type="hidden" name="status" value="active">
-            @endif
-            @error('status')
-                <span><strong>{{ $message }}</strong></span>
-            @enderror
-        </div>
+            <!-- updated_at -->
+            <div class="userDataColumnSection">
+                <div class="userDataColumn">
+                    <label>Учетная запись обновлена:</label>
+                    <div>{{ $context['updated_at'] }}</div>
+                </div>
+            </div>
 
-        <!-- name -->
-        <div>
-            <label>Имя:</label>
-            <input name="name" value="{{ old('name', $context['name']) }}">
-            @error('name')
-                <span><strong>{{ $message }}</strong></span>
-            @enderror
-        </div>
+            <!-- role -->
+            <div class="userDataColumnSection">
+                <div class="userDataColumn">
+                    <label>Роль:</label>
+                    <div>
+                        @if($context['role'] != 'root')
+                            <select name="role" class="editUserProfileSelect">
+                                <option value="admin" @if($context['role'] == 'admin') selected @endif>admin</option>
+                                <option value="writer" @if($context['role'] == 'writer') selected @endif>writer</option>
+                                <option value="reader" @if($context['role'] == 'reader') selected @endif>reader</option>
+                            </select>
+                        @else
+                            <div>root</div>
+                            <input type="hidden" name="role" value="root">
+                        @endif
+                    </div>
+                </div>
+                <div class="userDataErrorMessage">
+                    @error('role')
+                        <strong>{{ $message }}</strong>
+                    @enderror
+                </div>
+            </div>
 
-        <!-- surname -->
-        <div>
-            <label>Фамилия:</label>
-            <input name="surname" value="{{ old('surname', $context['surname']) }}">
-            @error('surname')
-                <span><strong>{{ $message }}</strong></span>
-            @enderror
-        </div>
+            <!-- status -->
+            <div class="userDataColumnSection">
+                <div class="userDataColumn">
+                    <label>Статус:</label>
+                    <div>
+                        @if($context['role'] != 'root' and $context['role'] != 'admin')
+                            <select name="status" class="editUserProfileSelect">
+                                <option value="active" @if($context['status'] == 'active') selected @endif>активен</option>
+                                <option value="ban" @if($context['status'] == 'ban') selected @endif>бан</option>
+                            </select>
+                        @else
+                            <div>активен</div>
+                            <input type="hidden" name="status" value="active">
+                        @endif
+                    </div>
+                </div>
+                <div class="userDataErrorMessage">
+                    @error('status')
+                        <strong>{{ $message }}</strong>
+                    @enderror
+                </div>
+            </div>
 
-        <!-- nickname -->
-        <div>
-            <label>Псевдоним:</label>
-            <input name="nickname" value="{{ old('nickname', $context['nickname']) }}">
-            @error('nickname')
-                <span><strong>{{ $message }}</strong></span>
-            @enderror
-        </div>
+            <!-- name -->
+            <div class="userDataColumnSection">
+                <div class="userDataColumn">
+                    <label>Имя:</label>
+                    <input name="name" value="{{ old('name', $context['name']) }}" class="loginInput">
+                </div>
+                <div class="userDataErrorMessage">
+                    @error('name')
+                        <strong>{{ $message }}</strong>
+                    @enderror
+                </div>
+            </div>
 
-        <!-- email -->
-        <div>
-            <label>Email:</label>
-            <input name="email" value="{{ old('email', $context['email']) }}">
-            @error('email')
-                <span><strong>{{ $message }}</strong></span>
-            @enderror
-        </div>
+            <!-- surname -->
+            <div class="userDataColumnSection">
+                <div class="userDataColumn">
+                    <label>Фамилия:</label>
+                    <input name="surname" value="{{ old('surname', $context['surname']) }}" class="loginInput">
+                </div>
+                <div class="userDataErrorMessage">
+                    @error('surname')
+                        <strong>{{ $message }}</strong>
+                    @enderror
+                </div>
+            </div>
 
-        <!-- change password -->
-        <div>
-            <input type="checkbox" name="editPassword" value="true"
-                @if(old('editPassword') == true)
-                    checked
-                @endif
-            >
-            <label>Изменить пароль:</label>
-            <input name="newPassword" value="{{ old('newPassword') }}" placeholder="не изменится">
-            @error('newPassword')
-                <span><strong>{{ $message }}</strong></span>
-            @enderror
-        </div>
+            <!-- nickname -->
+            <div class="userDataColumnSection">
+                <div class="userDataColumn">
+                    <label>Псевдоним:</label>
+                    <input name="nickname" value="{{ old('nickname', $context['nickname']) }}" class="loginInput">
+                </div>
+                <div class="userDataErrorMessage">
+                    @error('nickname')
+                        <strong>{{ $message }}</strong>
+                    @enderror
+                </div>
+            </div>
 
-        <!-- save -->
-        <input type="submit" value="Сохранить">
+            <!-- email -->
+            <div class="userDataColumnSection">
+                <div class="userDataColumn">
+                    <label>Email:</label>
+                    <input name="email" value="{{ old('email', $context['email']) }}" class="loginInput">
+                </div>
+                <div class="userDataErrorMessage">
+                    @error('email')
+                        <strong>{{ $message }}</strong>
+                    @enderror
+                </div>
+            </div>
+
+            <!-- change password -->
+            <div class="userDataColumnSection">
+                <div class="userDataColumn">
+                    <div>
+                        <input type="checkbox" name="editPassword" value="true"
+                            @if(old('editPassword') == true)
+                                checked
+                            @endif
+                        >
+                        <label>Изменить пароль:</label>
+                    </div>
+                    <input name="newPassword" value="{{ old('newPassword') }}" placeholder="не изменится" class="loginInput">
+                </div>
+                <div class="userDataErrorMessage">
+                    @error('newPassword')
+                        <span><strong>{{ $message }}</strong></span>
+                    @enderror
+                </div>
+            </div>
+        </div>
     </form>
 
-    <!-- cancel -->
-    <a href="{{ route('users.allProfiles') }}">Отмена</a>
+    <div class="saveCancel">
+        <button type="submit" form="editUserProfileForm" class="button htmlButtonStyleCorrection">Сохранить</button>
+        <a href="{{ route('users.allProfiles') }}" class="button">Отмена</a>
+    </div>
+</div>
 @endsection
