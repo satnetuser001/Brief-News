43f0a3328aa1c72b0260feed314c585ec294ce18
Anton Yurchenko <satnetuser001@gmail.com>
2023-09-27 13:43:25 +0300

    Added styles for the pages "Destroy User Profile Confirm".

--- resources/views/users/destroyUserProfileConfirm.blade.php
+++ resources/views/users/destroyUserProfileConfirm.blade.php
@@ -3,47 +3,78 @@
 @section('title', 'Удалить пользователя')
 
 @section('content')
-	<div>
+<!-- Reused styles from the "Edit User Profile" -->
+<div class="editUserProfile">
+	<div class="namePage">
 		<b>Вы действительно хотите удалить пользователя?</b>
 	</div>
 
-	<div>
-		ID: {{ $context->id }}
+	<div class="userBeingDeletedInformation">
+		<div class="userDataColumnSection">
+			<div class="userDataColumn">
+				<label>ID:</label>
+				<div>{{ $context->id }}</div>
+			</div>
+		</div>
+
+		<div class="userDataColumnSection">
+			<div class="userDataColumn">
+				<label>Имя:</label>
+				<div>{{ $context->name }}</div>
+			</div>
+		</div>
+
+		<div class="userDataColumnSection">
+			<div class="userDataColumn">
+				<label>Фамилия:</label>
+				<div>{{ $context->surname }}</div>
+			</div>
+		</div>
+
+		<div class="userDataColumnSection">
+			<div class="userDataColumn">
+				<label>Псевдоним:</label>
+				<div>{{ $context->nickname }}</div>
+			</div>
+		</div>
+
+		<div class="userDataColumnSection">
+			<div class="userDataColumn">
+				<label>Роль:</label>
+				<div>{{ $context->role }}</div>
+			</div>
+		</div>
+
+		<div class="userDataColumnSection">
+			<div class="userDataColumn">
+				<label>Статус:</label>
+				<div>{{ $context->status }}</div>
+			</div>
+		</div>
+
+		<div class="userDataColumnSection">
+			<div class="userDataColumn">
+				<label>Email:</label>
+				<div>{{ $context->email }}</div>
+			</div>
+		</div>
+
+		<div class="userDataColumnSection">
+			<div class="userDataColumn">
+				<label>Создан:</label>
+				<div>{{ $context->created_at }}</div>
+			</div>
+		</div>
 	</div>
 
-	<div>
-		Имя: {{ $context->name }}
+	<div class="saveCancel">
+		<form action="{{ route('users.destroyUserProfile', [$context->id]) }}" method="POST">
+			@csrf
+			@method('DELETE')
+			<input type="submit" value="Удалить" class="button htmlButtonStyleCorrection">
+		</form>
+
+		<a href="{{ route('users.allProfiles') }}" class="button">Отмена</a>
 	</div>
-
-	<div>
-		Фамилия: {{ $context->surname }}
-	</div>
-
-	<div>
-		Псевдоним: {{ $context->nickname }}
-	</div>
-
-	<div>
-		Роль: {{ $context->role }}
-	</div>
-
-	<div>
-		Статус: {{ $context->status }}
-	</div>
-
-	<div>
-		Email: {{ $context->email }}
-	</div>
-
-	<div>
-		Создан: {{ $context->created_at }}
-	</div>
-
-	<form action="{{ route('users.destroyUserProfile', [$context->id]) }}" method="POST">
-		@csrf
-		@method('DELETE')
-		<input type="submit" value="Удалить">
-	</form>
-
-	<a href="{{ route('users.allProfiles') }}">Отмена</a>
+</div>
 @endsection
