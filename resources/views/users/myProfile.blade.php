642b6419633450614001f48025ddbbaa788bfc89
Anton Yurchenko <satnetuser001@gmail.com>
2023-09-27 13:09:41 +0300

    Added styles for the pages "Edit My Profile".

--- resources/views/users/myProfile.blade.php
+++ resources/views/users/myProfile.blade.php
@@ -3,16 +3,17 @@
 @section('title', 'Мой профиль')
 
 @section('content')
-    <div>
+<!-- Reused styles from the "Edit User Profile" -->
+<div class="editUserProfile">
+    <div class="namePage">
         <b>Мой профиль</b>
     </div>
 
-    <div>
-        ID учетной записи: {{ $context['id'] }}
-    </div>
-
-
-    <form action="{{ route('users.updateMyProfile', [$context['id']]) }}" method="POST">
+    <form
+        id="editMyProfileForm"
+        action="{{ route('users.updateMyProfile', [$context['id']]) }}"
+        method="POST"
+    >
 
         @csrf
         @method('PATCH')
@@ -20,49 +21,76 @@
         <!-- User customization panel of rubrics and locale -->
         @include('includes.rubricsAndLocaleCustomizer')
 
-        <!-- email -->
-        <div>
-            <label>Email:</label>
-            <input name="email" value="{{ old('email', $context['email']) }}">
-            @error('email')
-                <span><strong>{{ $message }}</strong></span>
-            @enderror
-        </div>
+        <div class="userDataRow">
 
-        <!-- name -->
-        <div>
-            <label>Имя:</label>
-            <input name="name" value="{{ old('name', $context['name']) }}">
-            @error('name')
-                <span><strong>{{ $message }}</strong></span>
-            @enderror
-        </div>
+            <!-- ID -->
+            <div class="userDataColumnSection">
+                <div class="userDataColumn">
+                    <label>ID учетной записи:</label>
+                    <div>{{ $context['id'] }}</div>
+                </div>
+            </div>
 
-        <!-- surname -->
-        <div>
-            <label>Фамилия:</label>
-            <input name="surname" value="{{ old('surname', $context['surname']) }}">
-            @error('surname')
-                <span><strong>{{ $message }}</strong></span>
-            @enderror
-        </div>
+            <!-- email -->
+            <div class="userDataColumnSection">
+                <div class="userDataColumn">
+                    <label>Email:</label>
+                    <input name="email" value="{{ old('email', $context['email']) }}" class="loginInput">
+                </div>
+                <div class="userDataErrorMessage">
+                    @error('email')
+                        <span><strong>{{ $message }}</strong></span>
+                    @enderror
+                </div>
+            </div>
 
-        <!-- nickname -->
-        <div>
-            <label>Псевдоним:</label>
-            <input name="nickname" value="{{ old('nickname', $context['nickname']) }}">
-            @error('nickname')
-                <span><strong>{{ $message }}</strong></span>
-            @enderror
-        </div>
-        <div>
-            <a href="{{ route('users.editPassword') }}">Изменить пароль</a>
-        </div>
+            <!-- name -->
+            <div class="userDataColumnSection">
+                <div class="userDataColumn">
+                    <label>Имя:</label>
+                    <input name="name" value="{{ old('name', $context['name']) }}" class="loginInput">
+                </div>
+                <div class="userDataErrorMessage">
+                    @error('name')
+                        <span><strong>{{ $message }}</strong></span>
+                    @enderror
+                </div>
+            </div>
 
-        <!-- save -->
-        <input type="submit" value="Сохранить">
+            <!-- surname -->
+            <div class="userDataColumnSection">
+                <div class="userDataColumn">
+                    <label>Фамилия:</label>
+                    <input name="surname" value="{{ old('surname', $context['surname']) }}" class="loginInput">
+                </div>
+                <div class="userDataErrorMessage">
+                    @error('surname')
+                        <span><strong>{{ $message }}</strong></span>
+                    @enderror
+                </div>
+            </div>
+
+            <!-- nickname -->
+            <div class="userDataColumnSection">
+                <div class="userDataColumn">
+                    <label>Псевдоним:</label>
+                    <input name="nickname" value="{{ old('nickname', $context['nickname']) }}" class="loginInput">
+                </div>
+                <div class="userDataErrorMessage">
+                    @error('nickname')
+                        <span><strong>{{ $message }}</strong></span>
+                    @enderror
+                </div>
+            </div>
+
+            <a href="{{ route('users.editPassword') }}" class="editPasswordButton">Изменить пароль</a>
+
+        </div>
     </form>
 
-    <!-- cancel -->
-    <a href="{{ route('home') }}">Отмена</a>
+    <div class="saveCancel">
+        <button type="submit" form="editMyProfileForm" class="button htmlButtonStyleCorrection">Сохранить</button>
+        <a href="{{ route('home') }}" class="button">Отмена</a>
+    </div>
+</div>
 @endsection
